a7d3632c7343c91c743de41d0372a8312a8fb992
leon.maughan <leon.maughan@dtn.com>
2026-03-23 14:51:52 +0100

    Optimised for Android

--- static/sw.js
+++ static/sw.js
@@ -6,39 +6,40 @@ const urlsToCache = [
   '/static/manifest.json'
 ];
 
-// Install event - cache resources
+// Install event
 self.addEventListener('install', event => {
+  console.log('Service Worker installing');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
+        console.log('Caching app shell');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Fetch event - serve from cache when offline
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
 
-// Activate event - clean up old caches
+// Activate event
 self.addEventListener('activate', event => {
+  console.log('Service Worker activating');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
             return caches.delete(cache);
           }
         })
