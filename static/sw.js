901a84a1c57ca81d5bcf2edb01218b7b3a909aeb
Leon Maughan <leon44@gmail.com>
2026-03-23 14:46:20 +0100

    Optimised for Android

---
+++ static/sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'holy-island-causeway-v1';
+const urlsToCache = [
+  '/',
+  '/static/favicon.png',
+  '/static/favicon2x_square.png',
+  '/static/manifest.json'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
